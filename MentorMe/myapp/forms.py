f660dcf5f3e81185570080d548778a4dc93986f2
AvdikR <98918229+AvdikR@users.noreply.github.com>
2025-03-14 00:34:01 +0200

    Додано перші 4 сторінки веб-застосунку.

---
+++ MentorMe/myapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        if len(password1) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters long")
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is already in use")
+        return email
