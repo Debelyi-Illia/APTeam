c35686e853cea5b2b356a28e4169e9de079f251f
VladaBoholii <94959759+VladaBoholii@users.noreply.github.com>
2025-03-15 21:32:00 +0200

    register all models

--- MentorMe/myapp/admin.py
+++ MentorMe/myapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import User, Subject
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Subject)
+admin.site.register(Log)
+admin.site.register(LogType)
+admin.site.register(Advertisement)
+admin.site.register(Class)
